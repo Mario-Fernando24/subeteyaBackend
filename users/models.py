b368f00c10753a18f28852f24cce633746d6ea8f
Mario-Fernando24 <3115408069.mafe@gmail.com>
2025-08-08 00:27:34 -0500

    crear migraciones de la tabla usuario

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    #id autoincremental
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    #el email no se puede repetir, es unico
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    image = models.CharField(max_length=255, null=True, blank=False)
+    password = models.CharField(max_length=255)
+    notification_token = models.CharField(max_length=255, null=True)
+    create_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+
+
