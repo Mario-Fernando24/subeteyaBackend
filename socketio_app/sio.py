ffb1c54160c4fce4b1226cad5e1f8afba9b97cd3
Mario-Fernando24 <3115408069.mafe@gmail.com>
2025-11-10 23:51:13 -0500

    emitir la posicion del conductor con socket

--- socketio_app/sio.py
+++ socketio_app/sio.py
@@ -1,4 +1,5 @@
 import socketio
+import json
 
 sio = socketio.AsyncServer(
     async_mode="asgi",
@@ -13,3 +14,33 @@ async def connect(sid, environ):
 @sio.event
 async def disconnect(sid):
     print(f"Cliente desconectado: {sid}")
+
+@sio.event
+async def message(sid, data):
+   print(f'Datos del cliente en socket noooo: {sid}: {data}')
+   await sio.emit('new_message', data, to=sid)
+
+
+@sio.event
+async def change_drivers_position(sid, data):
+    try:
+        # Si 'data' viene como string JSON -> lo convertimos a dict
+        json_data = json.loads(data) if isinstance(data, str) else data
+
+        print(f'Emitió nueva posición en sockets {sid}: {json_data}')
+
+        await sio.emit(
+            'new_drivers_position',
+            {
+                'id_socket': sid,
+                'id': json_data["id"],
+                'latitud': json_data["lat"],
+                'longitud': json_data["lng"]
+            }
+        )
+
+    except json.JSONDecodeError as e:
+        print(f'ERROR al parsear JSON: {e}')
+    except Exception as e:
+        print(f'ERROR inesperado: {e}')
+
